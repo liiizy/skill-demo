14ce39513ec5b7c353608abb0e8dca1bcbcf638f
liiizy <amwong@ucsd.edu>
2022-02-03 16:08:21 -0800

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test 
     public void multiplication() {
-        assertEquals(10, SkillDemo.multiply(5, 4)); //fails
+        assertEquals(10, SkillDemo.multiply(5, 2)); //pass now
     }  
 }
